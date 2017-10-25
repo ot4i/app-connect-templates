63a93f9153e6405fb58c80cc2a7ed504d8a37a45
Mark Frost <FROSTMAR@uk.ibm.com>
2017-10-25 18:26:50 +0100

    move instructions under /resources

--- test/template-files-test.js
+++ test/template-files-test.js
@@ -50,7 +50,7 @@ describe('Template files', function () {
 
   it('must have a corresponding markdown file', function () {
     for (let template of templateMetadata.templates) {
-      let fileName = `./markdown/${template.name}_instructions.md`
+      let fileName = `./resources/markdown/${template.name}_instructions.md`
       expect(fs.existsSync(fileName), `Missing file ${fileName}`).equals(true)
     }
   })
